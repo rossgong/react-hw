524144725f08efdfc1045c23c2f2b43f2134343e
Ross Gongaware <ross@gongaware.org>
2019-04-11 11:28:34 -0400

    Updates

---
+++ precache-manifest.6ece1b93b4b7d44d5e2bfbe8cc7e21bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "25897e164ab465c8e1ee",
+    "url": "/react-hw/static/js/main.8f956383.chunk.js"
+  },
+  {
+    "revision": "38009e68cda529cc57a8",
+    "url": "/react-hw/static/js/runtime~main.e8db6a08.js"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/react-hw/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "5fef100a04b89a7f570eecd58fde476b",
+    "url": "/react-hw/index.html"
+  }
+];
